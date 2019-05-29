81046f74f5503ae160d68274412c83be5ecca399
Yaolegol <m160160@yandex.ru>
2019-05-30 02:03:33 +0700

    fix done

--- js/carousel.js
+++ js/carousel.js
@@ -7,66 +7,47 @@ $(document).ready(function () {
             autoWidth: true
         }
     );
-    $('.owl-stage').children('.active').addClass('shadow');
+
+    $('.owl-stage').children('.active').eq(0).addClass('shadow');
+    $('.owl-stage').children('.active').eq(1).addClass('shadow-middle-el');
+    $('.owl-stage').children('.active').eq(2).addClass('shadow');
+    
 
     //обработчик на свайп
-    owl.on('translated.owl.carousel', function () {
-        $('.owl-stage').children(':not([active])').removeClass('shadow');
-        $('.owl-stage').children('.active').addClass('shadow');
+    owl.on('translated.owl.carousel', function (e) {
+        $('.owl-stage').children(':not([active])').removeClass('shadow shadow-middle-el');
+        $('.owl-stage').children(':not([active])').find('.fifth-block-container-bottom-item__h4').removeClass('fifth-block-container-bottom-item__h4_activ');
+        $('.owl-stage').children(':not([active])').find('.fifth-block-container-bottom-item-read-more').removeClass('fifth-block-container-bottom-item-read-more_activ');
 
-        // $('.owl-next').hasClass('disabled') ? $('.next').addClass('fifth-block-container-buttons__btn_disabled') :
-        // $('.next').removeClass('fifth-block-container-buttons__btn_disabled');
-        
-        // $('.owl-prev').hasClass('disabled') ? $('.prev').addClass('fifth-block-container-buttons__btn_disabled') :
-        // $('.prev').removeClass('fifth-block-container-buttons__btn_disabled');
+        $('.owl-stage').children('.active').eq(0).addClass('shadow');
+        $('.owl-stage').children('.active').eq(1).addClass('shadow-middle-el');
+        $('.owl-stage').children('.active').eq(2).addClass('shadow');
+
+        $('.owl-stage').children('.active').eq(1).find('.fifth-block-container-bottom-item__h4').addClass('fifth-block-container-bottom-item__h4_activ');
+        $('.owl-stage').children('.active').eq(1).find('.fifth-block-container-bottom-item-read-more').addClass('fifth-block-container-bottom-item-read-more_activ');
     });
+
 
     //обработчики на кнопки
     let count = 0;
-    let maxCount = $('.owl-stage').children().length - 1; 
+    let maxCount = $('.owl-stage').children().length - 3; 
     
     $('.next').on('click', function () {
-        let h4 = $('.owl-stage').children('.active').eq(2).find('.fifth-block-container-bottom-item__h4');
-        $(h4).hasClass('fifth-block-container-bottom-item__h4_activ') ?
-        owl.trigger('next.owl.carousel') : null;        
-             
-        // $('.prev').hasClass('fifth-block-container-buttons__btn_disabled') ? $('.prev').removeClass('fifth-block-container-buttons__btn_disabled') : null;
-        // $('.owl-next').hasClass('disabled') ? $('.next').addClass('fifth-block-container-buttons__btn_disabled') : null;
+        owl.trigger('next.owl.carousel');
 
         count === maxCount - 1 ? $('.next').addClass('fifth-block-container-buttons__btn_disabled') :
         $('.prev').hasClass('fifth-block-container-buttons__btn_disabled') ? $('.prev').removeClass('fifth-block-container-buttons__btn_disabled') : null;
         
         count < maxCount ? count++ : null;
-        $('.owl-stage').children().eq(count - 1).find('.fifth-block-container-bottom-item__h4').removeClass('fifth-block-container-bottom-item__h4_activ');
-        $('.owl-stage').children().eq(count).find('.fifth-block-container-bottom-item__h4').addClass('fifth-block-container-bottom-item__h4_activ');
-
-        $('.owl-stage').children().eq(count - 1).find('.fifth-block-container-bottom-item-read-more').removeClass('fifth-block-container-bottom-item-read-more_activ');
-        $('.owl-stage').children().eq(count).find('.fifth-block-container-bottom-item-read-more').addClass('fifth-block-container-bottom-item-read-more_activ');
-
     });
-        
-
         
     
     $('.prev').on('click', function (event) {
-        let h4 = $('.owl-stage').children('.active').eq(0).find('.fifth-block-container-bottom-item__h4');
-        $(h4).hasClass('fifth-block-container-bottom-item__h4_activ') ?
-        owl.trigger('prev.owl.carousel') : null; 
-        
-        // $('.next').hasClass('fifth-block-container-buttons__btn_disabled') ? $('.next').removeClass('fifth-block-container-buttons__btn_disabled') : null;
-        // $('.owl-prev').hasClass('disabled') ? $('.prev').addClass('fifth-block-container-buttons__btn_disabled') : null;
-
+        owl.trigger('prev.owl.carousel');
+   
         count === 1 ? $('.prev').addClass('fifth-block-container-buttons__btn_disabled') :
         $('.next').hasClass('fifth-block-container-buttons__btn_disabled') ? $('.next').removeClass('fifth-block-container-buttons__btn_disabled') : null;
 
         count > 0 ? count-- : null;
-        $('.owl-stage').children().eq(count + 1).find('.fifth-block-container-bottom-item__h4').removeClass('fifth-block-container-bottom-item__h4_activ');
-        $('.owl-stage').children().eq(count).find('.fifth-block-container-bottom-item__h4').addClass('fifth-block-container-bottom-item__h4_activ'); 
-        
-        $('.owl-stage').children().eq(count + 1).find('.fifth-block-container-bottom-item-read-more').removeClass('fifth-block-container-bottom-item-read-more_activ');
-        $('.owl-stage').children().eq(count).find('.fifth-block-container-bottom-item-read-more').addClass('fifth-block-container-bottom-item-read-more_activ');       
     });
-
-
-
 });
