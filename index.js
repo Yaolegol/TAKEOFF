b2d864ca04be0b2318f089b09c6d1a8c35df8240
Yaolegol <m160160@yandex.ru>
2019-07-03 23:16:56 +0700

    server add

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('../TAKEOFF'));
+const router = require('./server/routes/router');
 
-app.get('/', (req, res) => {    
-    res.sendFile(__dirname + 'index.html');
-});
-app.get('/contacts', (req, res) => { 
-    res.send('<h1>Contacts</h1>');    
-    res.end();
-});
+app.use(express.static('./public'));
+
+app.use(router);
+
 
 app.listen(3000, () => console.log('Server start'));
---
+++ server/routes/router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {    
+    res.sendFile('index.html');   
+});
+
+router.get('/contacts', (req, res, next) => { 
+    res.send('<h1>Contacts</h1>');
+});
+
+module.exports = router;
