d93cbbabb18282353d3417ee4f60ddd58ab653bc
Oleg <m160160@yandex.ru>
2020-10-06 23:17:22 +0700

    add initial chek for disabled arrows

--- src/components/common/slider/index.js
+++ src/components/common/slider/index.js
@@ -21,6 +21,8 @@ class Slider {
             this.leftArrow = this.cliderControls.querySelector('.j-swiper-left-arrow');
             this.rightArrow = this.cliderControls.querySelector('.j-swiper-right-arrow');
 
+            this.checkArrowsEnabled();
+
             if(this.leftArrow && this.rightArrow) {
                 addEventListener(this.leftArrow, 'click', this.handleLeftArrowClick);
                 addEventListener(this.rightArrow, 'click', this.handleRightArrowClick);
@@ -30,10 +32,33 @@ class Slider {
 
     handleLeftArrowClick = () => {
         this.swiper.slidePrev();
+        console.log('isBeginning');
+        console.log(this.swiper.isBeginning);
+
+        if(this.swiper.isBeginning) {
+            this.leftArrow.classList.add('slider-controls__arrow-container_disabled');
+        } else {
+            this.rightArrow.classList.remove('slider-controls__arrow-container_disabled');
+        }
     }
 
     handleRightArrowClick = () => {
         this.swiper.slideNext();
+        console.log('isEnd');
+        console.log(this.swiper.isEnd);
+        if(this.swiper.isEnd) {
+            this.rightArrow.classList.add('slider-controls__arrow-container_disabled');
+        } else {
+            this.leftArrow.classList.remove('slider-controls__arrow-container_disabled');
+        }
+    }
+
+    checkArrowsEnabled = () => {
+        if(this.swiper.isBeginning) {
+            this.leftArrow.classList.add('slider-controls__arrow-container_disabled');
+        } else if(this.swiper.isEnd) {
+            this.rightArrow.classList.add('slider-controls__arrow-container_disabled');
+        }
     }
 }
 
