aba44d0fc89cfffbcafe6f8e2c0594d366a16d35
Oleg <m160160@yandex.ru>
2020-10-06 22:20:16 +0700

    add arrows controls

--- src/components/common/slider/index.js
+++ src/components/common/slider/index.js
@@ -1,9 +1,35 @@
+import {addEventListener} from '/common/helpers/events';
 import {addOnLoadListener} from '/common/helpers/load';
 import {createSwiper} from "/common/helpers/slider";
 
 class Slider {
     constructor(slider) {
+        this.swiperId = slider.dataset.swiperId;
+        this.leftArrow = document.querySelector(`.j-swiper-left-arrow[data-swiper-id=${this.swiperId}]`);
+        this.rightArrow = document.querySelector(`.j-swiper-right-arrow[data-swiper-id=${this.swiperId}]`);
+
+        this.init();
+    }
+
+    init = () => {
         this.swiper = createSwiper();
+
+        this.initArrows();
+    }
+
+    initArrows = () => {
+        if(this.leftArrow && this.rightArrow) {
+            addEventListener(this.leftArrow, 'click', this.handleLeftArrowClick);
+            addEventListener(this.rightArrow, 'click', this.handleRightArrowClick);
+        }
+    }
+
+    handleLeftArrowClick = () => {
+        this.swiper.slidePrev();
+    }
+
+    handleRightArrowClick = () => {
+        this.swiper.slideNext();
     }
 }
 
