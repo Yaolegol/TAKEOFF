a4f6465e25ecb8ff1d206503e2e5d1a4067bf35b
Oleg <m160160@yandex.ru>
2021-02-09 20:42:06 +0700

    add separate package.json for server

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(express.static(path.resolve(projectRootPath, 'dist')))
 
 app.get('/second', (req, res) => {
-    res.sendFile(path.resolve('dist','second.html'))
+    res.sendFile(path.resolve(projectRootPath, 'dist','second.html'))
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('dist','index.html'))
+    res.sendFile(path.resolve(projectRootPath, 'dist','index.html'))
 })
 
 app.listen(process.env.PORT || 8000, () => console.log('!!!!!!!!!!!Server start at localhost:8000!!!!!!!!!!!'));
