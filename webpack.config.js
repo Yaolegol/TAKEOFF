899663e037048ecfa8d13333e56798044528c702
Oleg <m160160@yandex.ru>
2021-02-13 01:20:28 +0700

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -38,14 +38,27 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg)$/,
                 use: [
-                    'file-loader',
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            context: 'src',
+                            name: '[path][name].[ext]',
+                            emitFile: false,
+                        }
+                    }
                 ],
             },
             // fonts
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    'file-loader',
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts',
+                        }
+                    }
                 ],
             },
             // handlebars
