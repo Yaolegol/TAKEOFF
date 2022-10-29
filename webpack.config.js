35fafd6a929a095cf8dabdb2d389ff2cfd18c619
Oleg <m160160@yandex.ru>
2022-10-29 17:08:55 +0700

    add docker + fix readme

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
     },
     output: {
         filename: "[name].[contenthash].bundle.js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "public"),
     },
     plugins: [
         new CopyPlugin({
