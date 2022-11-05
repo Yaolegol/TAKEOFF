241198af70228ba9cecb78515e12b9305805a43c
Oleg <m160160@yandex.ru>
2022-11-05 22:45:51 +0700

    add hero module

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,7 @@ import 'modules/common/layout/components/layout';
 import 'modules/pages/home/description';
 import 'modules/pages/home/description-short';
 import 'modules/pages/home/download-app';
+import 'modules/pages/home/hero';
 import 'modules/pages/home/products';
 import 'modules/pages/home/summary';
 import './index.less';
