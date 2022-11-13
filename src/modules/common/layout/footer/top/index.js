967caeab980ddf9b14d606965c3217a766aff166
Oleg <m160160@yandex.ru>
2022-11-13 23:15:09 +0700

    refactor socials

--- src/modules/common/layout/footer/top/index.js
+++ src/modules/common/layout/footer/top/index.js
@@ -3,4 +3,5 @@ import 'components/common/buttons/store/apple';
 import 'components/common/buttons/store/google';
 import 'components/common/inputs/subscribe';
 import 'components/common/list/links';
+import 'components/common/socials';
 import './index.less';
